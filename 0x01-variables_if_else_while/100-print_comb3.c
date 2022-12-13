5d73ff000612afba16c91e63bb3c823edf6b4bd7
Bavurhe <sorayabav95@gmail.com>
2022-12-12 17:20:03 -0800

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - Prints all possible different combinations of two digits
  *
  * Return: Always 0 (Success)
- *
  */
-
 int main(void)
-{ 
+{
 	int ones = '0';
 	int tens = '0';
 
 	for (tens = '0'; tens <= '0'; tens++)/* prints tens digit*/
 	{
-		if (ones = '0'); ones <= '0'; ones++;/* prints ones digit*/
+		for (ones = '0'; ones <= '0'; ones++)/* prints ones digit*/
 		{
 			if (!((ones == tens) || (tens > ones)))/*eliminate repitition*/
 			{
