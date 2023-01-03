a11826fdad731b1765aff072b7167affe7c2e83b
Bavurhe <sorayabav95@gmail.com>
2023-01-02 17:48:12 -0800

    Even more pointers, arrays, strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,6 +4,7 @@
  * _strspn - Entry point
  * @s: input
  * @accept: input
+ *
  * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
@@ -15,15 +16,15 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (r = 0; accept[r]; r++)
 		{
-			if(*s == accept[r])
+			if (*s == accept[r])
 			{
 				n++;
 				break;
 			}
-			else if(accept[r + 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
 	}
-	return(n);
+	return (n);
 }
