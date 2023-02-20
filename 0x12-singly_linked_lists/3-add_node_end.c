69d9899a15c3a0a1905f480ac1dda8ed88f815cd
Bavurhe <sorayabav95@gmail.com>
2023-02-20 03:32:15 -0800

    Singly linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,7 +18,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (str[len])
 		len++;
 
-	neww = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
@@ -38,4 +38,3 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	return (new);
 }
-
