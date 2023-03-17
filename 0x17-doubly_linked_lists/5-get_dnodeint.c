3ee6532d3f8d362b39e43b7cbe1800d9710d3f52
Bavurhe <sorayabav95@gmail.com>
2023-03-16 18:47:26 -0700

    double linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,10 +6,10 @@
  * @index: index of the node to return
  *
  * Return: if the node does not exist, return NULL
- * 	where index is the index of the node, starting from 0
+ *	where index is the index of the node, starting from 0
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{ 
+{
 	dlistint_t *current;
 	unsigned int i;
 
